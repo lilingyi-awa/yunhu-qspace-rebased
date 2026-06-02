190a11a969231a33ae5e6af60b8743fc0f7206ea
Li lingyi <145204041+lilingyi-awa@users.noreply.github.com>
2026-06-02 20:06:23 +0800

    Update overview links.

--- packages/frontend/src/pages/about.overview.vue
+++ packages/frontend/src/pages/about.overview.vue
@@ -31,13 +31,14 @@ SPDX-License-Identifier: AGPL-3.0-only
 				<template #icon><i class="ti ti-info-circle"></i></template>
 				{{ i18n.ts.aboutMisskey }}
 			</FormLink>
-			<FormLink v-if="instance.repositoryUrl || instance.providesTarball" :to="instance.repositoryUrl || `/tarball/misskey-${version}.tar.gz`" external>
+			<FormLink to="https://yhfx.jwznb.com/share?key=UGHUaiFnRlLU&ts=1780400878" external>
+				<template #icon><i class="ti ti-messages"></i></template>
+				交流群
+			</FormLink>
+			<FormLink to="/@7261230/pages/opensource">
 				<template #icon><i class="ti ti-code"></i></template>
 				{{ i18n.ts.sourceCode }}
 			</FormLink>
-			<MkInfo v-else warn>
-				{{ i18n.ts.sourceCodeIsNotYetProvided }}
-			</MkInfo>
 		</div>
 	</FormSection>
 
@@ -71,16 +72,11 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<template #icon><i class="ti ti-user-shield"></i></template>
 					<template #default>{{ i18n.ts.impressum }}</template>
 				</FormLink>
-				<MkFolder v-if="instance.serverRules.length > 0">
-					<template #icon><i class="ti ti-checkup-list"></i></template>
-					<template #label>{{ i18n.ts.serverRules }}</template>
-					<ol class="_gaps_s" :class="$style.rules">
-						<li v-for="item in instance.serverRules" :key="item" :class="$style.rule">
-							<div :class="$style.ruleText" v-html="item"></div>
-						</li>
-					</ol>
-				</MkFolder>
-				<FormLink v-if="instance.tosUrl" :to="instance.tosUrl" external>
+				<FormLink to="/@7261230/pages/term-of-services">
+					<template #icon><i class="ti ti-license"></i></template>
+					<template #default>{{ i18n.ts.serverRules }}</template>
+				</FormLink>
+				<FormLink to="/@7261230/pages/term-of-services">
 					<template #icon><i class="ti ti-license"></i></template>
 					<template #default>{{ i18n.ts.termsOfService }}</template>
 				</FormLink>
@@ -88,7 +84,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<template #icon><i class="ti ti-shield-lock"></i></template>
 					<template #default>{{ i18n.ts.privacyPolicy }}</template>
 				</FormLink>
-				<FormLink v-if="instance.feedbackUrl" :to="instance.feedbackUrl" external>
+				<FormLink to="/channels/an05k2kq5knq00ap">
 					<template #icon><i class="ti ti-message"></i></template>
 					<template #default>{{ i18n.ts.feedback }}</template>
 				</FormLink>
@@ -111,17 +107,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 			</FormSplit>
 		</FormSection>
 	</MkSuspense>
-
-	<FormSection>
-		<template #label>Well-known resources</template>
-		<div class="_gaps_s">
-			<FormLink to="/.well-known/host-meta" external>host-meta</FormLink>
-			<FormLink to="/.well-known/host-meta.json" external>host-meta.json</FormLink>
-			<FormLink to="/.well-known/nodeinfo" external>nodeinfo</FormLink>
-			<FormLink to="/robots.txt" external>robots.txt</FormLink>
-			<FormLink to="/manifest.json" external>manifest.json</FormLink>
-		</div>
-	</FormSection>
 </div>
 </template>
 
